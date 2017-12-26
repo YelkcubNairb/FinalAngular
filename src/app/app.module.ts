dfca0da66a4c6386406f93546fabcb7cc5978f75
YelkcubNairb <btbuckley15@gmail.com>
2017-12-26 13:12:10 -0500

    Commited part Ang

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { FormsModule } from '@angular/forms'; // NOTE ilker added for 2 way data binding between template and component
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router'; // NOTE ilker added to use routes
 import { AppComponent } from './app.component';
-
-
+import { TeacherListComponent } from './components/teacher-list/teacher-list.component';
+import { TeacherListDataService } from './services/teacher-list-data.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TeacherListComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpModule,
   ],
-  providers: [],
+  providers: [TeacherListDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
